154779a2f3d20636ef1f7f580464ed20582aed64
ishidakenji <ishida@jidaikobo.com>
2026-08-17 16:57:23 +0900

    Revert "common.js内のjQuery.animateによるスムーススクロール処理をコメントアウト"
    This reverts commit 0926902af39191c7ab4888ebdc47ca038c926d07.

--- common/js/common.js
+++ common/js/common.js
@@ -22,7 +22,6 @@ $win.on('load resize', function() {
   });
 }); 
 
-  /*
   $('a[href^=#]').click(function(){
     var speed = 500;
     var href= $(this).attr("href");
@@ -31,18 +30,17 @@ $win.on('load resize', function() {
     $("html, body").animate({scrollTop:position}, speed, "swing");
     return false;
   });
-  */
   
  $(function(){
 	$(".accordion li a").on("click", function() {
 		$(this).next().slideToggle();	
-		// active‚ª‘¶İ‚·‚éê‡
+		// activeãŒå­˜åœ¨ã™ã‚‹å ´åˆ
 		if ($(this).children(".accordion_icon").hasClass('active')) {			
-			// active‚ğíœ
+			// activeã‚’å‰Šé™¤
 			$(this).children(".accordion_icon").removeClass('active');				
 		}
 		else {
-			// active‚ğ’Ç‰Á
+			// activeã‚’è¿½åŠ 
 			$(this).children(".accordion_icon").addClass('active');			
 		}			
 	});
